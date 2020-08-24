4fd4e355f47db41c847beb1be5ee04fcd3366b1d
deirn <foreirn@gmail.com>
2020-08-24 14:15:33 +0700

    1.16.1

--- src/main/java/badasintended/monopoly/mixin/ItemEntityMixin.java
+++ src/main/java/badasintended/monopoly/mixin/ItemEntityMixin.java
@@ -1,11 +1,23 @@
 package badasintended.monopoly.mixin;
 
 import badasintended.monopoly.Monopoly;
+import badasintended.monopoly.UnifyConfig;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+import java.util.UUID;
 
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin extends EntityMixin {
