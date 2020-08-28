0de96237b7e90500fce457b0a58a085b38c8d888
deirn <foreirn@gmail.com>
2020-08-28 15:59:53 +0700

    allow script to update existing config

--- config_generator.py
+++ config_generator.py
@@ -7,27 +7,64 @@ import os
 def clear(): os.system('cls' if os.name == 'nt' else 'clear')
 
 
-configMap = {}
+config_map = {}
 
-i = 0
+if os.path.exists("monopoly.json"):
+    config_map = json.load(open("monopoly.json"))
+
 clear()
-print("Total: 0")
 while True:
+    print("Help: comma (,) to delete existing value")
+    print("----------------------------------------")
     tag = input("Tag: ").lower().strip()
     if not tag:
         break
-    target = input("Target: ").lower().strip()
-    nbt = input("Allow nbt ? [y/n] (n):").lower().strip() == "y"
+    if tag in config_map:
+        prev = config_map[tag] if type(config_map[tag]) == str else config_map[tag]["target"]
+        target = input(f"Target [,] ({prev}): ").lower().strip()
+        if target == ",":
+            config_map.pop(tag)
+            clear()
+            continue
+        if not target:
+            target = prev
+    else:
+        target = input("Target: ").lower().strip()
+    if not target:
+        break
+
+    prev_nbt = False
+    prev_exclude = []
+    if tag in config_map:
+        prev = config_map[tag]
+        if type(prev) == dict:
+            if "nbt" in prev:
+                prev_nbt = prev["nbt"]
+            if "exclude" in prev:
+                if type(prev["exclude"]) == str:
+                    prev_exclude.append(prev["exclude"])
+                else:
+                    prev_exclude.extend(prev["exclude"])
+
+    nbt_input = input(f"Allow nbt ? [y/n] ({prev_nbt}): ").lower().strip()
+    nbt = nbt_input == "y" if nbt_input else prev_nbt
     exclude = []
     j = 0
     while True:
-        j = j + 1
-        ex = input(f"Exclude {j}: ").lower().strip()
+        default = "" if j >= len(prev_exclude) else " [,] (" + prev_exclude[j] + ")"
+        ex = input(f"Exclude {j}{default}: ").lower().strip()
+        if ex == "," and default:
+            j = j + 1
+            continue
         if not ex:
-            break
-        exclude.append(ex)
+            if not default:
+                break
+            ex = prev_exclude[j]
+        if ex not in exclude:
+            exclude.append(ex)
+        j = j + 1
     if not nbt and not exclude:
-        configMap[tag] = target
+        config_map[tag] = target
     else:
         config = {"target": target}
         if nbt:
@@ -37,12 +74,10 @@ while True:
                 config["exclude"] = exclude[0]
             else:
                 config["exclude"] = exclude
-        configMap[tag] = config
+        config_map[tag] = config
     clear()
-    i = i + 1
-    print(f"Total: {i}")
 
-jsonConfig = json.dumps(configMap, indent=2)
+jsonConfig = json.dumps(config_map, indent=2)
 
 clear()
 
